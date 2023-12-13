3c06ce4af8ed11ecaf7e6015e977ddf8b4b5523d
Tinatin <omurovatinatin@gmail.com>
2023-12-13 14:21:59 +0600

    project init

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import '../styles/main.scss';
+import withAppProviders from './withAppProviders';
+import BrowserRouter from './setup/core/browserRouter';
+import Layout from './config';
+
+const App = () => {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default withAppProviders(App)();
---
+++ src/app/config/routesConfig.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Contribute from "../page/Contribute";
+
+
+const routes = [
+  {
+    path: "/",
+    element: <Contribute />,
+  },
+];
+
+export default routes;
